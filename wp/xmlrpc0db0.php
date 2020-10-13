a30f672b87b537bb558556b4b6ae79ed9ed5be89
T_okita <t.remolo.okita.0813@gmail.com>
2020-10-13 14:57:27 +0900

    reusページをコミットします

---
+++ wp/xmlrpc0db0.php
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <apis>
+	    </apis>
+  </service>
+</rsd>
